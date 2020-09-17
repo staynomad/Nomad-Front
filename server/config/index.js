62404672bfc5359ba2a8c184733899b3e951c72d
Seamus Le <seamust.le@gmail.com>
2020-09-17 14:38:03 -0700

    removed port from config/index.js

--- server/config/index.js
+++ server/config/index.js
@@ -1,6 +1,5 @@
 module.exports = {
     environment: process.env.NODE_ENV || "development",
-    port: process.env.PORT || 8080,
     DATABASE_URI: process.env.DATABASE_URI,
     jwtConfig: {
         secret: process.env.JWT_SECRET,
