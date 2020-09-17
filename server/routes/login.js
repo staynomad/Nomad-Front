d41ea5672019d0d1fe01de503f6c9c99b1ce46a6
Seamus Le <seamust.le@gmail.com>
2020-09-17 12:07:25 -0700

    added login to routes and created utils file in root. added gitignore, env, and envexample file.

---
+++ server/routes/login.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+/* User Login */
+router.post('/', asyncHandler(async (req, res, next) => {
+  
+}));
+
+module.exports = router;
