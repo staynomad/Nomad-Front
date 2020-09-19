5a13601baf54bc8ebd80a32525ec39b45645e228
Andrea Sekito <andrea@Chonkers-Computer.local>
2020-09-18 21:41:22 -0700

    font change to welcome back

--- client/src/components/authentication/login.component.js
+++ client/src/components/authentication/login.component.js
@@ -6,7 +6,7 @@ const Login = () => {
     <div className="login-content">
       <div className="login-form">
         <form action="/login_success" className="form">
-          <h2>welcome back</h2>
+          <h2 style={{ color: "#31473b", fontSize: "2em" }}>welcome back</h2>
           <div>
             <input
               type="email"
@@ -23,13 +23,18 @@ const Login = () => {
               className="input login-input"
             />
           </div>
-          <div>
+          <div style={{ margin: "2% 0 3%" }}>
             <button className="btn green">log in</button>
           </div>
-          <div></div>
+          <div className="separate"></div>
           <div>
             <span>don't have an account? </span>
-            <a href="/SignUp">sign up</a>
+            <a
+              href="/SignUp"
+              style={{ color: "green", textDecoration: "none" }}
+            >
+              sign up
+            </a>
           </div>
         </form>
       </div>
