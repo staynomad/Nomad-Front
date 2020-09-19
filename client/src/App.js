6cc48e6663f84eb863bb887141dbe4172cef2f56
Andrea Sekito <andrea@Chonkers-Computer.local>
2020-09-18 21:50:40 -0700

    services page set up and linked

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Home from "./components/homePage/home.component";
 import Login from "./components/authentication/login.component";
 import Signup from "./components/authentication/signup.component";
 import Contact from "./components/subpages/contact.component.js";
+import Services from "./components/subpages/services.component.js";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 //to add more items just copy the format and add the route path. look at navbar component to see where the path is currently set to
@@ -18,6 +19,7 @@ function App() {
           <Route path="/ContactUs" exact component={Contact} />
           <Route path="/Login" exact component={Login} />
           <Route path="/SignUp" exact component={Signup} />
+          <Route path="/Services" exact component={Services} />
         </Switch>
 
         <Footer />
