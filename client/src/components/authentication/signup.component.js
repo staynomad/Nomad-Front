f00ebcc0ad3c12afd552a94e91d20e3f2d3c7039
Andrea Sekito <andrea@Chonkers-Computer.local>
2020-09-22 19:44:43 -0700

    sign up handeling and session assigning with token

--- client/src/components/authentication/signup.component.js
+++ client/src/components/authentication/signup.component.js
@@ -8,34 +8,67 @@ const Signup = () => {
     password: "",
   });
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch("http://localhost:8080/login", {
+      method: "POST",
+      mode: "no-cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: userSignup,
+    }).then((res) => {
+      if (res.status === 422) {
+        console.log(res.body.errors);
+      }
+
+      if (res.status === 201) {
+        window.sessionStorage.accessToken = res.body.token;
+      }
+    });
+  };
+
   return (
     <div id="signup-content">
       <div class="login-form signup-form">
-        <h2 className="services-title">create an account</h2>
+        <h2 className="services-title signup-title">create an account</h2>
         <form className="form signup-form">
-          <input
-            type="text"
-            placeholder="your name"
-            className="input login-input border-transparent-white"
-          />
           <input
             type="email"
             placeholder="your email"
-            className="input login-input border-transparent-white"
+            className="input login-input"
+            onChange={(e) =>
+              setUserSignup({ ...userSignup, email: e.target.value })
+            }
+          />
+          <input
+            type="text"
+            placeholder="your name"
+            className="input login-input"
+            onChange={(e) =>
+              setUserSignup({ ...userSignup, name: e.target.value })
+            }
           />
           <input
             type="password"
             placeholder="your password"
-            className="input login-input border-transparent-white"
+            className="input login-input"
+            onChange={(e) =>
+              setUserSignup({ ...userSignup, password: e.target.value })
+            }
           />
           <input
             type="submit"
             value="create your account"
             className="btn green"
+            onClick={(e) => handleSubmit(e)}
           />
         </form>
         <div>
-          already have an account? <span>log in</span>
+          already have an account?{" "}
+          <a href="/Login" style={{ color: "green", textDecoration: "none" }}>
+            log in
+          </a>
         </div>
       </div>
     </div>
