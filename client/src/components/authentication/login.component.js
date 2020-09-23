79b4326b928d765b49145162e67e5fb8f4857ad9
Andrea Sekito <andrea@Chonkers-Computer.local>
2020-09-22 18:44:45 -0700

    login handling 400 error and 200 error, set local storage with token from server

--- client/src/components/authentication/login.component.js
+++ client/src/components/authentication/login.component.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./login.css";
 
 const Login = () => {
+  const [userLogin, setUserLogin] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    fetch("http://localhost:8080/login", {
+      method: "POST",
+      mode: "no-cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: userLogin,
+    }).then((res) => {
+      if (res.status === 401) {
+        alert("Incorrect email or password");
+      }
+
+      if (res.status === 200) {
+        window.sessionStorage.accessToken = res.body.token;
+      }
+    });
+  };
+
   return (
     <div className="login-content">
       <div className="login-form">
-        <form action="/login_success" className="form">
+        <form action="/login" className="form">
           <h2 style={{ color: "#31473b", fontSize: "2em" }}>welcome back</h2>
           <div>
             <input
@@ -13,6 +38,10 @@ const Login = () => {
               name="email"
               placeholder="your email"
               className="input login-input"
+              onChange={(e) =>
+                setUserLogin({ ...userLogin, email: e.target.value })
+              }
+              required={true}
             />
           </div>
           <div>
@@ -21,10 +50,16 @@ const Login = () => {
               name="password"
               placeholder="your password"
               className="input login-input"
+              onChange={(e) =>
+                setUserLogin({ ...userLogin, password: e.target.value })
+              }
+              required={true}
             />
           </div>
           <div style={{ margin: "2% 0 3%" }}>
-            <button className="btn green">log in</button>
+            <button className="btn green" onClick={(e) => handleLogin(e)}>
+              log in
+            </button>
           </div>
           <div className="separate"></div>
           <div>
