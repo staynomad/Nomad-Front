b01de6a135260a113b40dd09df55eed8c5e3e7f0
Andrea Sekito <andrea@Chonkers-Computer.local>
2020-10-04 15:49:48 -0700

    testing out listings component

--- client/src/components/matches/listings.component.js
+++ client/src/components/matches/listings.component.js
@@ -5,13 +5,37 @@ const Listings = () => {
   const [listings, setListings] = useState([]);
 
   useEffect(() => {
-    setListings([{ name: "100 Acre Room" }]);
+    // setListings([{ name: "100 Acre Room" }]);
+    handleReq("/listings", "GET")
+      .then((data) => {
+        return data.json();
+      })
+      .then((res) => {
+        if (res.errors) {
+          alert(res.errors[0]);
+        }
+        console.log(res.body);
+        if (res.body) {
+          setListings(res.body);
+        }
+      });
   }, []);
 
   return (
     <div>
       {listings.map((listing) => (
-        <div>Listing: {listing.name}</div>
+        <div className='listing-item'>
+          <div className='listing-information'>
+            <div>Listing: {listing.description}</div>
+            <div>
+              Details: {listing.details.beds} {listing.details.baths}{" "}
+              {listing.details.maxpeople}
+            </div>
+            <div>rating: {listing.rating.user}</div>
+            <div>Price: {listing.price}</div>
+          </div>
+          <div>Click to see more!</div>
+        </div>
       ))}
     </div>
   );
