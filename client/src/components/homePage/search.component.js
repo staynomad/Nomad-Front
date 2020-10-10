576edce9e53e4a994ff5ccd4da36bc04591f9202
Aiden Szeto <aszeto35@gmail.com>
2020-10-09 17:28:42 -0700

    fixed overlapping text homepage

--- client/src/components/homePage/search.component.js
+++ client/src/components/homePage/search.component.js
@@ -6,9 +6,11 @@ export default class Search extends Component {
     return (
       <div className="overallsearch">
         <div className="toptext">
-          the
-          <span style={{ color: "#02b188" }}> future</span>
-          <div>of flexible rentals</div>
+          <div className="toprow">
+            the
+            <span style={{ color: "#02b188" }}> future</span>
+          </div>
+          of flexible rentals
         </div>
         <input className="inputtextbox" type="text" placeholder="  city" />
         <a href="/OurLocations">
