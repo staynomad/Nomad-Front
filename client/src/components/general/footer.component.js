28bc20e9471372c15c9d77208c8e928e77e31d4d
Seamus Le <seamust.le@gmail.com>
2020-10-20 19:27:55 -0700

    used material ui for footer (stays at the bottom now)

--- client/src/components/general/footer.component.js
+++ client/src/components/general/footer.component.js
@@ -1,21 +1,38 @@
 import React, { Component } from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import Toolbar from '@material-ui/core/Toolbar';
 import "./footer.css";
 //Calling on this folder from other folders to make the pages
 
-export default class Footer extends Component {
-  render() {
-    return (
-      <ul className='footer'>
-        <li>
-          <a href='/ContactUs'>CONTACT US</a>
-        </li>
-        <li>
-          <a href='/AboutUs'>ABOUT US</a>
-        </li>
-        <li>
-          <a href='/OurLocations'>OUR LOCATIONS</a>
-        </li>
-      </ul>
-    );
+/* Material-UI appbar styling */
+const useStyles = makeStyles((theme) => ({
+  root: {
+    boxShadow: "none",
+    color: "black",
+  },
+  linkItems: {
+    fontSize: "14px",
+    fontWeight: "bold",
+    margin: "0 12px 0 12px",
+  },
+  spacing: {
+    flexGrow: 1,
   }
+}));
+
+export default function Footer () {
+  const classes = useStyles();
+
+  return (
+    <BottomNavigation className={classes.root} color="transparent" position="absolute">
+      <Toolbar>
+        <span className={classes.spacing} />
+        <a className={classes.linkItems} href='/ContactUs'>CONTACT US</a>
+        <a className={classes.linkItems} href='/AboutUs'>ABOUT US</a>
+        <a className={classes.linkItems} href='/OurLocations'>OUR LOCATIONS</a>
+        <span className={classes.spacing} />
+      </Toolbar>
+    </BottomNavigation>
+  );
 }
