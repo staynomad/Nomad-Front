328aee438cfffbd94ee2960ebfb54e315eea1eb2
Viki Mancoridis <vmancoridis@gmail.com>
2020-10-26 08:28:43 -0600

    Fixed bugs in MyAccount page

--- client/src/components/myAccount/menu.component.js
+++ client/src/components/myAccount/menu.component.js
@@ -1,11 +1,8 @@
 import React, { Component } from 'react'
 import { Grid, Menu, Segment } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
-import Matches from "../matches/matches.component"
 import Questionnaire from "../matches/questionnaire.component"
-// import Listings from "../matches/listings.component"
 import Profile from "./profile.component"
-import Explore from './explore.component'
 import Settings from "./settings.component"
 
 export default class LeftMenu extends Component {
@@ -27,20 +24,18 @@ export default class LeftMenu extends Component {
     console.log(this.state)
     switch(this.state.render) {
       case 'profile':
-        return <Profile />;
-      case 'explore':
-        return <Explore />;
-      // case 'my listings':
-      //   return <Listings />;
+        return (
+          <div>
+            <Profile />
+            <Questionnaire />
+          </div>
+        );
+      case 'my listings':
+        return;
       case 'settings':
         return <Settings />;
-      case 'matches': 
-        return <Matches />;
-      case 'roommate questionnaire':
-        return <Questionnaire />;
-
       default:
-        return <Matches />;
+        return;
     }
   }
 
@@ -48,6 +43,8 @@ export default class LeftMenu extends Component {
     const { activeItem } = this.state
 
     return (
+      <div>
+      <br /><br /><br /><br /><br /> {/* can implement this more cleanly with styling */}
       <Grid className="container">
         <Grid.Column width={3}>
           <Menu fluid vertical tabular>
@@ -58,33 +55,21 @@ export default class LeftMenu extends Component {
               onClick={this.handleItemClick}
             />
             <Menu.Item
-              name='explore'
-              active={activeItem === 'explore'}
-              compname='explore'
-              onClick={this.handleItemClick}
-            />
-            {/* <Menu.Item
               name='my listings'
               active={activeItem === 'my listings'}
               compname='my listings'
               onClick={this.handleItemClick}
-            /> */}
+            />
+            <Menu.Item
+              name="renter dashboard"
+              active={activeItem === 'renter dashboard'}
+              compname="renter dashboard"
+              onClick={this.handleItemClick}
+            />
             <Menu.Item
               name='settings'
               active={activeItem === 'settings'}
               compname='settings'
-              onClick={this.handleItemClick}
-            />
-            <Menu.Item 
-              name='roommate questionnaire'
-              active={activeItem === 'roommate questionnaire'}
-              compname='roommate questionnaire'
-              onClick={this.handleItemClick}
-            />
-            <Menu.Item 
-              name='matches'
-              active={activeItem === 'matches'}
-              compname='matches'
               onClick={this.handleItemClick}
             />
           </Menu>
@@ -95,6 +80,7 @@ export default class LeftMenu extends Component {
           </Segment>
         </Grid.Column>
       </Grid>
+    </div>
     )
   }
 }
