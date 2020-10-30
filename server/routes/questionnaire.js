53d84aad15964becb44fd0a06c1335f65bb34f7a
Viki Mancoridis <vmancoridis@gmail.com>
2020-10-30 11:15:12 -0600

    working-- edit previous questionnaire responses on the backend if users have already submitted their response

--- server/routes/questionnaire.js
+++ server/routes/questionnaire.js
@@ -36,38 +36,12 @@ router.post(
       userId,
     } = req.body;
 
-    // newQuestionnaire-- based off of what the user filled out in the form
-    const newQuestionnaire = await new Questionnaire({
-      name,
-      email,
-      stateUS,
-      numberOfRoommates,
-      bedtime,
-      petPreference,
-      workFromHome,
-      workFromHomePreference,
-      fridayNight,
-      cleaning,
-      cleaningPreference,
-      cooking,
-      cookingPreference,
-      friends,
-      friendsPreference,
-      smoke,
-      personalityType,
-      selfDescription,
-      roommateStory,
-      covidStory,
-      hobbies,
-      userId
-    }).save();
-
     // previous questionnaire-- null if it does not exist
     const prevQuestionnaire = await Questionnaire.findOne({userId: req.params.id});
 
     // if user is updating their questionnaire preferences
     if (prevQuestionnaire) {
-      Questionnaire.findOneAndUpdate({userId: req.params.id}, {$set: newQuestionnaire})
+      Questionnaire.findOneAndUpdate({userId: req.params.id}, {$set: req.body})
         .then(() => console.log('success in updating previous questionnaire response!'))
         .catch(err => console.log('this is your error: ', err))
     } 
@@ -75,7 +49,33 @@ router.post(
     // if user is submitting a questionnaire for the first time
     else {
       try {
-        // request is created. ****** needs more validation ******
+        // newQuestionnaire-- based off of what the user filled out in the form
+        const newQuestionnaire = await new Questionnaire({
+          name,
+          email,
+          stateUS,
+          numberOfRoommates,
+          bedtime,
+          petPreference,
+          workFromHome,
+          workFromHomePreference,
+          fridayNight,
+          cleaning,
+          cleaningPreference,
+          cooking,
+          cookingPreference,
+          friends,
+          friendsPreference,
+          smoke,
+          personalityType,
+          selfDescription,
+          roommateStory,
+          covidStory,
+          hobbies,
+          userId
+        }).save();
+
+        // request is created. need ui for validation
         res.status(201).json ({
           newQuestionnaire
         });
