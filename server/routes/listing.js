f9ff5ee1e1912ab617b4f4174011a89dad01c55f
Seamus Le <seamust.le@gmail.com>
2020-11-02 22:56:06 -0800

    added route to handle getting all user listings and deleting a listing by id

--- server/routes/listing.js
+++ server/routes/listing.js
@@ -63,4 +63,51 @@ router.get("/", async (req, res) => {
   }
 });
 
+/* Get all listings belonging to user */
+router.get("/byEmail", requireUserAuth, async (req, res) => {
+  try {
+    const userListings = await Listing.find({email: req.user.email});
+    if (!userListings) {
+      res.status(404).json({
+        errors: ["There are currently no listings! Please try again later."],
+      });
+    } else {
+      res.status(201).json({
+        userListings,
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      errors: ["Error occurred while getting listings. Please try again!"],
+    });
+  }
+});
+
+/* Delete listing by id */
+router.delete("/remove/:listingId", requireUserAuth, async (req, res) => {
+  try {
+    const listing = await Listing.findOne({
+      _id: req.params.listingId,
+      email: req.user.email,
+    });
+
+    if (!listing) {
+      res.status(500).json({
+        errors: ["Listing was not found. Please try again!"],
+      });
+    } else {
+      listing.remove()
+      res.status(200).json({
+        message: ["Listing was removed."],
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      errors: ["Error occurred while attempting to remove listing. Please try again!"],
+    });
+  }
+});
+
 module.exports = router;
