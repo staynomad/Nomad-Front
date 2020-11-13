1badc6fbd71918c9164ee6a6190a9109faf6bd88
aszeto <aidenszeto@g.ucla.edu>
2020-11-13 10:56:16 -0800

    added validation for booked dates

--- server/routes/reservation.js
+++ server/routes/reservation.js
@@ -5,7 +5,7 @@ const Reservation = require("../models/reservation.model");
 const Listing = require("../models/listing.model")
 const { requireUserAuth } = require("../utils");
 
-
+// Create a reservation
 router.post(
     "/createReservation",
     async (req, res) => {
@@ -14,26 +14,42 @@ router.post(
             const listingInfo = await Listing.findOne({
                 _id: listing
             })
+
             const availableStart = new Date(listingInfo.available[0])
             const availableEnd = new Date(listingInfo.available[1])
             const reservationStart = new Date(days[0])
             const reservationEnd = new Date(days[1])
+
             if (reservationStart.getTime() < availableStart.getTime() || reservationEnd.getTime() > availableEnd.getTime()) {
                 return res.status(400).json({
                     "errors": "Selected days are invalid. Please try again."
                 })
             }
+
+            for (let i = 0; i < listingInfo.booked.length; i++) {
+              const bookedStart = new Date(listingInfo.booked[i].start)
+              const bookedEnd = new Date(listingInfo.booked[i].end)
+              if ((reservationStart.getTime() >= bookedStart.getTime() && reservationStart.getTime() <= bookedEnd.getTime()) || (reservationEnd.getTime() >= bookedStart.getTime() && reservationEnd.getTime() <= bookedEnd.getTime())) {
+                  return res.status(400).json({
+                      "errors": "Selected days are invalid. Please try again."
+                  })
+              }
+            }
+
             const newReservation = await new Reservation({
                 email,
                 listing,
                 active: true,
                 days,
             }).save();
+
             const bookedDays = {
               start: days[0],
               end: days[1]
             }
+
             const bookedListing = await Listing.findOneAndUpdate({ _id: listing }, { $push: { booked: bookedDays } })
+
             res.status(201).json({
               "message": "Reservation created successfully"
             });
@@ -48,6 +64,7 @@ router.post(
     }
 )
 
+// Change this (and reservation model) to get by id
 router.get(
     "/getByEmail/:email",
     async (req, res) => {
@@ -72,6 +89,7 @@ router.get(
     }
 )
 
+// Get reservations corresponding to listingId
 router.get(
     "/getByListing/:listing",
     async (req, res) => {
@@ -96,6 +114,7 @@ router.get(
     }
 )
 
+// Delete a specific reservation by reservationId
 router.delete(
     "/delete/:listing",
     async (req, res) => {
@@ -120,6 +139,7 @@ router.delete(
     }
 )
 
+// Change this to deactivate by reservationId
 router.post(
     "/deactivate/:listing",
     async (req, res) => {
