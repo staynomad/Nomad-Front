ec7d10a3b6e5a9a23132939514528644166706bc
Andrew <andrewliu08@gmail.com>
2020-11-21 17:55:58 -0500

    add checkin checkout buttons to reservation card

--- client/src/components/reservations/reservationCard.component.js
+++ client/src/components/reservations/reservationCard.component.js
@@ -1,5 +1,19 @@
 import React, { useState } from 'react';
+import Button from '@material-ui/core/Button';
 import { NavLink } from 'react-router-dom';
+import { withStyles } from '@material-ui/core/styles';
+
+const CustomButton = withStyles((theme) => ({
+  root: {
+    color: "#00B183",
+    backgroundColor: "transparent",
+    border: "2px solid #00B183",
+    borderRadius: "8px",
+    font: "inherit",
+    fontSize: "16px",
+    fontWeight: "normal",
+  },
+}))(Button);
 
 const ReservationCard = (props) => {
   const [open, setOpen] = useState(false);
@@ -15,19 +29,16 @@ const ReservationCard = (props) => {
         <div className='reservation-information'>
           <div className='reservation-image'>reservation image here</div>
           <div>
-            <b>{reservation.listing.description}</b>
-          </div>
-          <div>
-            <b>Details:</b> {reservation.listing.details.beds > 1 ? `${reservation.listing.details.beds} beds` : `${reservation.listing.details.beds} bed`}  {reservation.listing.details.baths > 1 ? `${reservation.listing.details.baths} baths` : `${reservation.listing.details.baths} bath`}
-          </div>
-          <div>
-            <b>Rating:</b> {reservation.listing.rating.user} / 5
-            </div>
-          <div>
-            <b>Starting Price:</b> ${reservation.listing.price}
+            <b>Reservation Date:</b> `${reservation.date[0]} - ${reservation.date[1]}`
           </div>
         </div>
       </NavLink>
+      {props.userSession && props.userSession.userId === reservation.userId ? (
+        <CustomButton>Change</CustomButton>
+      ) : null}
+      {props.userSession && props.userSession.userId === reservation.userId ? (
+        <CustomButton>Cancel</CustomButton>
+      ) : null}
     </div>
   )
 };
