ca5e45f57ac39cf3ef97d0a8909bd7cfd5cf49ae
aszeto <aidenszeto@g.ucla.edu>
2020-12-10 21:38:03 -0800

    testing for calendar integration

--- src/components/createListing/importCalendar.component.js
+++ src/components/createListing/importCalendar.component.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react'
 import { app } from '../../utils/axiosConfig.js'
+var ical2json = require("ical2json");
 
 class ImportCalendar extends Component {
-  
+  componentDidMount() {
+    const url = 'https://www.airbnb.com/calendar/ical/46943704.ics?s=e2cf1d44f063299b1060fea2da160a8d'
+    fetch(url)
+      .then( r => r.text() )
+      .then( t => console.log(t))
+    var output = ical2json.convert('https://www.airbnb.com/calendar/ical/46943704.ics?s=e2cf1d44f063299b1060fea2da160a8d');
+    console.log(output)
+  }
+  render() {
+    return (
+      <div>this works</div>
+    )
+  }
 }
 
 export default ImportCalendar
