59db3299b4972f49de2407d65a2a1209a3425767
aszeto <aidenszeto@g.ucla.edu>
2020-12-14 15:13:22 -0800

    proxy to bypass access control server response

--- src/components/createListing/importCalendar.component.js
+++ src/components/createListing/importCalendar.component.js
@@ -1,16 +1,35 @@
 import React, { Component } from 'react'
 import { app } from '../../utils/axiosConfig.js'
-var ical2json = require("ical2json");
+import axios from 'axios'
 
 class ImportCalendar extends Component {
   componentDidMount() {
-    const url = 'https://www.airbnb.com/calendar/ical/46943704.ics?s=e2cf1d44f063299b1060fea2da160a8d'
-    fetch(url)
-      .then( r => r.text() )
-      .then( t => console.log(t))
-    var output = ical2json.convert('https://www.airbnb.com/calendar/ical/46943704.ics?s=e2cf1d44f063299b1060fea2da160a8d');
-    console.log(output)
+    /*fetch('https://www.airbnb.com/calendar/ical/46943704.ics?s=e2cf1d44f063299b1060fea2da160a8d', {
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      }
+    })
+    .then(t => {
+      console.log(t)
+    })
+    .catch( err => console.log(err))*/
+
+    /*var icalfile = new XMLHttpRequest();
+    if (icalfile) {
+      icalfile.open("GET", "https://www.airbnb.com/calendar/ical/46943704.ics?s=e2cf1d44f063299b1060fea2da160a8d", true);
+      icalfile.withCredentials = false
+      icalfile.send(null);
+      console.log(icalfile.getResponseHeader("*"))
+    }*/
+
+    // Using allorigins proxy to bypass airbnb access-control-allow-origin server response header
+    axios.get("https://api.allorigins.win/raw?url=https://www.airbnb.com/calendar/ical/46943704.ics?s=e2cf1d44f063299b1060fea2da160a8d")
+    .then((res) => {
+      console.log(res.data) // raw text response from ical file
+    })
   }
+
   render() {
     return (
       <div>this works</div>
