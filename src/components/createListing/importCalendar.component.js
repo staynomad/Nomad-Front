c7dd6360082938121550e4b03bba3fa8fdaae5d9
aszeto <aidenszeto@g.ucla.edu>
2020-12-14 15:15:12 -0800

    documented request

--- src/components/createListing/importCalendar.component.js
+++ src/components/createListing/importCalendar.component.js
@@ -4,26 +4,8 @@ import axios from 'axios'
 
 class ImportCalendar extends Component {
   componentDidMount() {
-    /*fetch('https://www.airbnb.com/calendar/ical/46943704.ics?s=e2cf1d44f063299b1060fea2da160a8d', {
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-      }
-    })
-    .then(t => {
-      console.log(t)
-    })
-    .catch( err => console.log(err))*/
-
-    /*var icalfile = new XMLHttpRequest();
-    if (icalfile) {
-      icalfile.open("GET", "https://www.airbnb.com/calendar/ical/46943704.ics?s=e2cf1d44f063299b1060fea2da160a8d", true);
-      icalfile.withCredentials = false
-      icalfile.send(null);
-      console.log(icalfile.getResponseHeader("*"))
-    }*/
-
-    // Using allorigins proxy to bypass airbnb access-control-allow-origin server response header
+    // temporary solution: using allorigins proxy to bypass airbnb access-control-allow-origin server response header
+    // https://gist.github.com/jimmywarting/ac1be6ea0297c16c477e17f8fbe51347
     axios.get("https://api.allorigins.win/raw?url=https://www.airbnb.com/calendar/ical/46943704.ics?s=e2cf1d44f063299b1060fea2da160a8d")
     .then((res) => {
       console.log(res.data) // raw text response from ical file
