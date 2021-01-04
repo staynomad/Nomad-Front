82d421b54618b909ed318dc44679d98ad5738c85
aszeto <aidenszeto@g.ucla.edu>
2021-01-03 17:04:58 -0800

    about us setup

--- src/components/homePage/footer.component.js
+++ src/components/homePage/footer.component.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
+import "./home.css"
 
 class navbar extends Component {
   render() {
@@ -13,7 +14,8 @@ class navbar extends Component {
             <a href="https://www.instagram.com/vhomesgroup/" ><img src="/images/instagram.png" alt="" /></a>
         	</div>
           <div className="spacer_xs"></div>
-          <NavLink to="/contact" className="wow fadeInUp" data-wow-delay="0.2s">Contact Us</NavLink>
+          <NavLink to="/contact" className="wow fadeInUp footer-item" data-wow-delay="0.2s">Contact Us</NavLink>
+          <NavLink to="/about" className="wow fadeInUp footer-item" data-wow-delay="0.2s">About Us</NavLink>
           <h5 className="wow fadeInUp" data-wow-delay="0.2s">© 2020 Vhomes - All Rights Reserved. </h5>
         </div>
       </footer>
