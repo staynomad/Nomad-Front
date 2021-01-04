4a2eabccdc9785aee25d5aec6968bb517a01a9a7
aszeto <aidenszeto@g.ucla.edu>
2021-01-03 17:04:58 -0800

    about us setup

--- src/components/homePage/footer.component.js
+++ src/components/homePage/footer.component.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
+import "./home.css"
 
 class navbar extends Component {
   render() {
@@ -7,14 +8,15 @@ class navbar extends Component {
       <footer>
         <div className="footer-content">
 
-        	<div className="footer-social-media">
+        	<div className="footer-social-media wow fadeInUp" data-wow-delay="0.2s">
         		<a href="https://www.linkedin.com/company/vhomesgroup"><img src="/images/linkedin.png" alt="" /></a>
         		<a href="https://www.facebook.com/rentvhomes/" ><img src="/images/facebook.png" alt="" /></a>
             <a href="https://www.instagram.com/vhomesgroup/" ><img src="/images/instagram.png" alt="" /></a>
         	</div>
           <div className="spacer_xs"></div>
-          <NavLink to="/contact">Contact Us</NavLink>
-          <h5>© 2021 Vhomes - All Rights Reserved. </h5>
+          <NavLink to="/contact" className="wow fadeInUp footer-item" data-wow-delay="0.2s">Contact Us</NavLink>
+          <NavLink to="/about" className="wow fadeInUp footer-item" data-wow-delay="0.2s">About Us</NavLink>
+          <h5 className="wow fadeInUp" data-wow-delay="0.2s">© 2020 Vhomes - All Rights Reserved. </h5>
         </div>
       </footer>
     );
