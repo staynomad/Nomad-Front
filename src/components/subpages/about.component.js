f7725256f28d0df8aab950417bad5a39c19bff9b
Nishant Balaji <nishantb1130@gmail.com>
2021-01-11 02:21:00 -0800

    temporary about us page

--- src/components/subpages/about.component.js
+++ src/components/subpages/about.component.js
@@ -4,7 +4,9 @@ export default class About extends Component {
   render() {
     return (
       <div className="container">
+      <p>
         Our founder Mike Vilardo knows the struggle of being a mom & pop property manager on Airbnb. Starting by renting out his patio at his Chicago home on Airbnb, Mike was able to scale up to managing several properties across a few major cities. He felt the pain of managing the day to day operations of the places, customer service, all the while having to market and acquire customers on Airbnb. This was all going ok until Covid-19 hit and the properties started to struggle. However, there was a silver lining as traveling nurses and emergency workers began to book longer term stays at Mike’s properties. At that moment, Mike and the team realized that there was something there, and that we should make affordable housing options available to all traveling workers on a budget for when they need it the most. Why just limit it to our own properties? This is when the VHomes platform was born.
+        </p>
       </div>
     )
   }
