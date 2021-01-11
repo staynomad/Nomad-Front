7ed8e78090fe8c4eafd0a8c03fcde433af4acac4
Nishant Balaji <nishantb1130@gmail.com>
2021-01-11 02:11:45 -0800

    footer change

--- src/components/homePage/footer.component.js
+++ src/components/homePage/footer.component.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
+import "./home.css"
 
 class navbar extends Component {
   render() {
@@ -13,9 +14,9 @@ class navbar extends Component {
             <a href="https://www.instagram.com/vhomesgroup/" ><img src="/images/instagram.png" alt="" /></a>
         	</div>
           <div className="spacer_xs"></div>
-          <NavLink to="/contact" className="footer-item">Contact Us</NavLink>
-          <NavLink to="/about" className="footer-item">About Us</NavLink>
-          <h5>© 2021 Vhomes - All Rights Reserved. </h5>
+          <NavLink to="/contact" className="wow fadeInUp footer-item" data-wow-delay="0.2s">Contact Us</NavLink>
+          <NavLink to="/about" className="wow fadeInUp footer-item" data-wow-delay="0.2s">About Us</NavLink>
+          <h5 className="wow fadeInUp" data-wow-delay="0.2s">© 2020 Vhomes - All Rights Reserved. </h5>
         </div>
       </footer>
     );
