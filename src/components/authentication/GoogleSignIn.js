0b9dab82abbc352a7d925c93cbd352d05e0e89f3
Jack Palaia <jack@jackpalaia.com>
2021-01-28 17:44:10 -0500

    move google login to below login button

--- src/components/authentication/GoogleSignIn.js
+++ src/components/authentication/GoogleSignIn.js
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from "react";
 // import { app } from '../../utils/axiosConfig.js'
-import GoogleLogin from 'react-google-login'
-import { submitGoogleLogin } from "../../redux/actions/authActions"
+import GoogleLogin from "react-google-login";
+import { submitGoogleLogin } from "../../redux/actions/authActions";
 import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 
-const clientID = '799198545666-1s1bu2dvg1n4hj9hgpbjsecjhrbkbhc8.apps.googleusercontent.com'
+const clientID =
+  "799198545666-1s1bu2dvg1n4hj9hgpbjsecjhrbkbhc8.apps.googleusercontent.com";
 
-const GoogleSignIn = props => {
-
-  const handleLogin = googleData => props.submitGoogleLogin(googleData)
+const GoogleSignIn = (props) => {
+  const handleLogin = (googleData) => props.submitGoogleLogin(googleData);
 
   return (
     <>
@@ -18,28 +18,28 @@ const GoogleSignIn = props => {
         buttonText="Log in with Google"
         onSuccess={handleLogin}
         onFailure={handleLogin}
-        cookiePolicy={'single_host_origin'}
+        cookiePolicy={"single_host_origin"}
       />
+      <div className="spacer_s"></div>
     </>
-  )
-}
+  );
+};
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   const stateToReturn = { ...state, loading: state.Loading.loading };
-  if (state.Login.userInfo) stateToReturn['userSession'] = state.Login.userInfo.session;
-  if (state.Errors.authErrors) stateToReturn['authErrors'] = state.Errors.authErrors;
+  if (state.Login.userInfo)
+    stateToReturn["userSession"] = state.Login.userInfo.session;
+  if (state.Errors.authErrors)
+    stateToReturn["authErrors"] = state.Errors.authErrors;
   return stateToReturn;
 };
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch) => {
   return {
     submitGoogleLogin: (googleData) => dispatch(submitGoogleLogin(googleData)),
   };
 };
 
-export default withRouter(connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(
-  GoogleSignIn
-));
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(GoogleSignIn)
+);
