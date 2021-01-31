da330f418ac0264f852bfcf14573e1ca5b23cfb7
MikePodo <michaelspodolsky@gmail.com>
2021-01-30 18:01:40 -0800

    add columnization, lighter box-shadows, and remove white space at bottom of each card

--- src/components/matches/matches.component.js
+++ src/components/matches/matches.component.js
@@ -3,12 +3,12 @@ import { withRouter } from "react-router-dom";
 import Filter from "./filter.component";
 import Roommates from "./roommate/roommates.component";
 import Listings from "./listing/listings.component";
-import Search from "../homePage/search.component"
+import Search from "../homePage/search.component";
 import "./matches.css";
 import "./listing/explore.css";
 
 const Matches = (props) => {
-  const { history } = props
+  const { history } = props;
   const [seen, setSeen] = useState(false);
   var roommateView = false;
   const [listingView, setListingView] = useState(true);
@@ -37,14 +37,18 @@ const Matches = (props) => {
   }, [location.search]);
 
   return (
-    <div id='matches-page'>
+    <div id="matches-page">
       <div className="spacer_l"></div>
       <div class="spacer_xl"></div>
       <div className="wow fadeInUp" data-wow-delay="0.4s">
-        <div className="row gap large" >
-          <div className="col-sm-12 col-md-12 col-lg-12" >
+        <div className="row gap large">
+          <div className="col-sm-12 col-md-12 col-lg-12">
             <div className=" wow fadeInUp" data-wow-delay="0.5s">
-              <img src="images/banner.svg" alt="banner" className="banner center-image" />
+              <img
+                src="images/banner.svg"
+                alt="banner"
+                className="banner center-image"
+              />
             </div>
           </div>
         </div>
@@ -52,7 +56,9 @@ const Matches = (props) => {
       <div className="spacer_s"> </div>
       <div className="wow fadeInUp" data-wow-delay="0.5s">
         <Search history={history} />
-        <div>Click on a listing to see more information!</div>
+        <div className="click-for-more">
+          Click on a listing to see more information!
+        </div>
         {/*<button className="filter btn green" onClick={() => {
           setRoommateView(!roommateView);
           setListingView(false)
