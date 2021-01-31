942c402ba4ee41f8e99b3259e67e96bf1f725aea
Jack Palaia <jack@jackpalaia.com>
2021-01-31 13:49:26 -0500

    add error handling to redux action

--- src/redux/actions/authActions.js
+++ src/redux/actions/authActions.js
@@ -1,51 +1,68 @@
-import axios from 'axios';
+import axios from "axios";
 import handleReq from "../../utils/fetchRequest";
-import { push } from 'connected-react-router';
+import { push } from "connected-react-router";
 import { setAuthError } from "./errorActions";
 import { setLoadingFalse, setLoadingTrue } from "./loadingActions";
 
 /* Types */
-export const SET_USER_SESSION = 'VHomes/search/SET_USER_SESSION'
-export const REMOVE_USER_SESSION = 'VHomes/search/REMOVE_USER_SESSION'
+export const SET_USER_SESSION = "VHomes/search/SET_USER_SESSION";
+export const REMOVE_USER_SESSION = "VHomes/search/REMOVE_USER_SESSION";
 
 /* Actions */
-const setUserSession = (isHost, token, userId) => ({ type: SET_USER_SESSION, isHost, token, userId });
-const removeUserSession = () => ({ type: REMOVE_USER_SESSION })
+const setUserSession = (isHost, token, userId) => ({
+  type: SET_USER_SESSION,
+  isHost,
+  token,
+  userId,
+});
+const removeUserSession = () => ({ type: REMOVE_USER_SESSION });
 
 /* Fetch Calls and functions */
-export const submitLogin = userLogin => async dispatch => {
-    dispatch(setLoadingTrue());
-    const headers = {
-        "Content-Type": "application/json",
-    };
-    const loginRes = await handleReq("/login", "POST", headers, userLogin);
+export const submitLogin = (userLogin) => async (dispatch) => {
+  dispatch(setLoadingTrue());
+  const headers = {
+    "Content-Type": "application/json",
+  };
+  const loginRes = await handleReq("/login", "POST", headers, userLogin);
 
-    if (loginRes && loginRes.status === 200) {
-        const { isHost, token, userId } = loginRes.data;
-        axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-        dispatch(setLoadingFalse());
-        dispatch(setUserSession(isHost, token, userId));
-    } else {
-        const { errors } = loginRes.data;
-        dispatch(setLoadingFalse());
-        dispatch(setAuthError(errors));
-    }
+  if (loginRes && loginRes.status === 200) {
+    const { isHost, token, userId } = loginRes.data;
+    axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+    dispatch(setLoadingFalse());
+    dispatch(setUserSession(isHost, token, userId));
+  } else {
+    const { errors } = loginRes.data;
+    dispatch(setLoadingFalse());
+    dispatch(setAuthError(errors));
+  }
 };
 
-export const submitGoogleLogin = googleData => async dispatch => {
-    dispatch(setLoadingTrue())
-    const headers = {
-        "Content-Type": "application/json",
-    }
-    const loginRes = await handleReq("/googleLogin", "POST", headers, JSON.stringify({ token: googleData.tokenId }))
+export const submitGoogleLogin = (googleData) => async (dispatch) => {
+  dispatch(setLoadingTrue());
+  const headers = {
+    "Content-Type": "application/json",
+  };
+  const loginRes = await handleReq(
+    "/googleLogin",
+    "POST",
+    headers,
+    JSON.stringify({ token: googleData.tokenId })
+  );
 
-    const { isHost, token, userId } = loginRes.data
-    dispatch(setLoadingFalse())
-    dispatch(setUserSession(isHost, token, userId))
-}
+  if (loginRes && loginRes.status === 200) {
+    const { isHost, token, userId } = loginRes.data;
+    axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+    dispatch(setLoadingFalse());
+    dispatch(setUserSession(isHost, token, userId));
+  } else {
+    const { errors } = loginRes.data;
+    dispatch(setLoadingFalse());
+    dispatch(setAuthError(errors));
+  }
+};
 
-export const logoutUser = () => dispatch => {
-    axios.defaults.headers.common['Authorization'] = null;
-    dispatch(removeUserSession());
-    dispatch(push(`/`));
-}
+export const logoutUser = () => (dispatch) => {
+  axios.defaults.headers.common["Authorization"] = null;
+  dispatch(removeUserSession());
+  dispatch(push(`/`));
+};
