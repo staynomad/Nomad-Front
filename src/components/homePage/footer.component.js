7b95ac815a998e020d54cc9adc461145cab99c09
MikePodo <michaelspodolsky@gmail.com>
2021-02-08 16:07:49 -0800

    change vhomes images and text to nomad

--- src/components/homePage/footer.component.js
+++ src/components/homePage/footer.component.js
@@ -59,7 +59,7 @@ class navbar extends Component {
             About Us
           </NavLink>
           <h5 className="wow fadeInUp" data-wow-delay="0.2s">
-            © 2020 Vhomes - All Rights Reserved.{" "}
+            © 2020 NomΛd - All Rights Reserved.{" "}
           </h5>
         </div>
       </footer>
