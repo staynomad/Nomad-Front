dfbfcf2fc781afda52827467f087ae36b8173e27
MikePodo <michaelspodolsky@gmail.com>
2021-03-05 18:37:46 -0800

    create horizontal listings scroll component and add to public profile page

--- src/components/matches/listing/HorizontalListingCard.component.js
+++ src/components/matches/listing/HorizontalListingCard.component.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+import guestImage from "../../../assets/img/guest.svg";
+import bedImage from "../../../assets/img/bed.svg";
+import bathImage from "../../../assets/img/bath.svg";
+
 const HorizontalListingCard = ({ listing }) => {
   return (
     <NavLink to={"/listing/" + listing._id} className="horizontal-listing-card">
@@ -16,19 +20,19 @@ const HorizontalListingCard = ({ listing }) => {
         <h3>{listing.title}</h3>
         <div className="horizontal-listing-card-details-container">
           <div>
-            <img src="images/guest.svg" alt="guest" />
+            <img src={guestImage} alt="guest" />
             <h4>{`${listing.details.maxpeople} Guest${
               listing.details.maxpeople === "1" ? "" : "s"
             }`}</h4>
           </div>
           <div>
-            <img src="images/bed.svg" alt="bed" />
+            <img src={bedImage} alt="bed" />
             <h4>{`${listing.details.beds} Bed${
               listing.details.beds === "1" ? "" : "s"
             }`}</h4>
           </div>
           <div>
-            <img src="images/bath.svg" alt="bath" />
+            <img src={bathImage} alt="bath" />
             <h4>{`${listing.details.baths} Bath${
               listing.details.baths === "1" ? "" : "s"
             }`}</h4>
