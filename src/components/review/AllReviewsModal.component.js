1867f5da59f4b0563ebebc724d21aa10bfbdbdfc
MikePodo <72575260+MikePodo@users.noreply.github.com>
2021-03-12 15:15:58 -0800

    create reviews graph UI and integration

--- src/components/review/AllReviewsModal.component.js
+++ src/components/review/AllReviewsModal.component.js
@@ -1,11 +1,70 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import LinearProgress from "@material-ui/core/LinearProgress";
 
 import KeyboardBackspaceIcon from "@material-ui/icons/KeyboardBackspace";
 import StarBorderIcon from "@material-ui/icons/StarBorder";
 import StarIcon from "@material-ui/icons/Star";
+import CheckBoxOutlineBlankIcon from "@material-ui/icons/CheckBoxOutlineBlank";
+import CheckBoxIcon from "@material-ui/icons/CheckBox";
 
 const AllReviewsModal = ({ reviews, closeModal, openPostReview }) => {
+  const [fiveStars, setFiveStars] = useState(0);
+  const [fiveStarsAverage, setFiveStarsAverage] = useState(0);
+  const [fourStars, setFourStars] = useState(0);
+  const [fourStarsAverage, setFourStarsAverage] = useState(0);
+  const [threeStars, setThreeStars] = useState(0);
+  const [threeStarsAverage, setThreeStarsAverage] = useState(0);
+  const [twoStars, setTwoStars] = useState(0);
+  const [twoStarsAverage, setTwoStarsAverage] = useState(0);
+  const [oneStar, setOneStar] = useState(0);
+  const [oneStarAverage, setOneStarAverage] = useState(0);
+
+  console.log(reviews);
+
+  useEffect(() => {
+    let total5 = 0;
+    let total4 = 0;
+    let total3 = 0;
+    let total2 = 0;
+    let total1 = 0;
+
+    reviews.forEach((review) => {
+      switch (review.stars) {
+        case 5:
+          total5 = total5 + 1;
+          break;
+        case 4:
+          total4 = total4 + 1;
+          break;
+        case 3:
+          total3 = total3 + 1;
+          break;
+        case 3:
+          total3 = total3 + 1;
+          break;
+        case 2:
+          total2 = total2 + 1;
+          break;
+        case 1:
+          total1 = total1 + 1;
+          break;
+        default:
+          return;
+      }
+      setFiveStars(total5);
+      setFourStars(total4);
+      setThreeStars(total3);
+      setTwoStars(total2);
+      setOneStar(total1);
+
+      setFiveStarsAverage((total5 / reviews.length) * 100);
+      setFourStarsAverage((total4 / reviews.length) * 100);
+      setThreeStarsAverage((total3 / reviews.length) * 100);
+      setTwoStarsAverage((total2 / reviews.length) * 100);
+      setOneStarAverage((total1 / reviews.length) * 100);
+    });
+  }, []);
+
   const getStars = () => {
     let ratings = [];
     for (let props in reviews) {
@@ -44,7 +103,9 @@ const AllReviewsModal = ({ reviews, closeModal, openPostReview }) => {
       <div className="all-reviews-top-container">
         <div className="all-reviews-write-container">
           <div className="rating-container">{getStars()}</div>
-          <h2>({reviews.length} Reviews)</h2>
+          <h2>{`(${reviews.length} Review${
+            reviews.length === 1 ? "" : "s"
+          })`}</h2>
           <button
             onClick={() => {
               closeModal();
@@ -54,8 +115,44 @@ const AllReviewsModal = ({ reviews, closeModal, openPostReview }) => {
             Write a review
           </button>
         </div>
+        <div className="all-reviews-graph-container">
+          <div className="all-reviews-graph-row-container">
+            <CheckBoxIcon />
+            <p>5</p>
+            <StarIcon className="all-reviews-graph-star" />
+            <LinearProgress variant="determinate" value={fiveStarsAverage} />
+            <p>{fiveStars}</p>
+          </div>
+          <div className="all-reviews-graph-row-container">
+            <CheckBoxIcon />
+            <p>4</p>
+            <StarIcon className="all-reviews-graph-star" />
+            <LinearProgress variant="determinate" value={fourStarsAverage} />
+            <p>{fourStars}</p>
+          </div>
+          <div className="all-reviews-graph-row-container">
+            <CheckBoxIcon />
+            <p>3</p>
+            <StarIcon className="all-reviews-graph-star" />
+            <LinearProgress variant="determinate" value={threeStarsAverage} />
+            <p>{threeStars}</p>
+          </div>
+          <div className="all-reviews-graph-row-container">
+            <CheckBoxIcon />
+            <p>2</p>
+            <StarIcon className="all-reviews-graph-star" />
+            <LinearProgress variant="determinate" value={twoStarsAverage} />
+            <p>{twoStars}</p>
+          </div>
+          <div className="all-reviews-graph-row-container">
+            <CheckBoxIcon />
+            <p>1</p>
+            <StarIcon className="all-reviews-graph-star" />
+            <LinearProgress variant="determinate" value={oneStarAverage} />
+            <p>{oneStar}</p>
+          </div>
+        </div>
       </div>
-      {/* <LinearProgress variant="determinate" value={50} /> */}
     </div>
   );
 };
