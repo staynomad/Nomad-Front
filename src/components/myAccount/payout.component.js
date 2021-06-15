88f476553864af4a725473e0532b78c1236abaed
Nishant Balaji <nishantb1130@gmail.com>
2021-06-14 17:06:59 -0700

    start express dashboard link

--- src/components/myAccount/payout.component.js
+++ src/components/myAccount/payout.component.js
@@ -1,21 +1,43 @@
 import React, { useState } from 'react'
+// import { useSelector, useDispatch } from "react-redux";
 import './payout.css'
 import handleReq from "../../utils/fetchRequest";
 
 const Payout = () => {
    const [stripeConnected, setConnect] = useState(false);
+//    const user = useSelector((state) => state.User);
+//    const email = user.userInfo.email;
+   let id = ""; 
 
-   const handleClick = async () => {
-      const body = {
-
-      };
-
+   // Put users into the workflow to connect their stripe accounts. 
+   const handleStripeClick = async () => {
+      const body = {};
       const url = "/payouts/setup";
+      const data = {};
+ 
+      let res = await handleReq(
+            url,
+            "POST",
+            body, 
+            data
+      )
+      if(res.status === 200){
+            setConnect(true);
+            id = "hi" // res.data.id;
+            // window.location.href = res.data.link;
+      }
+      else if(res.status === 500){
+            return <h1>Something went wrong. Please try again later!</h1>
+      }            
+   };
+
+   // Give users a link to their Express Dashboards
+   const handleDashboardClick = async () => {
+      const body = {};
+      const url = "/payouts/express";
       const data = {
-
+            userId: id,
       };
-
-      // TODO: connect stripe properly in the BE, payout.js. 
       let res = await handleReq(
             url,
             "POST",
@@ -24,22 +46,22 @@ const Payout = () => {
       )
       if(res.status === 200){
             window.location.href = res.data.link;
-            setConnect(true);
       }
       else if(res.status === 500){
             return <h1>Something went wrong. Please try again later!</h1>
       }
- 
-      //             
-   };
+   }
 
    return (
          <>
          {!stripeConnected ? 
          <div className="container">
-         <div onClick={() => handleClick()} className="stripe-connect" ><span>Connect with</span></div>
+         <div onClick={() => handleStripeClick()} className="stripe-connect" ><span>Connect with</span></div>
          </div> :
-         null}
+         <div> 
+            <div> You're connected to stripe!  </div> 
+            <div onClick={()=> handleDashboardClick()}>Click here to view your dashboard</div>
+         </div>} {/* Click <a href={}>here</a> */}
          </>
    ) 
 }
